4eaa4701a17cf777092987d7e1843bc50c0dc5f8
edroptyul1-svg <edroptyul1@gmail.com>
2026-05-26 13:00:39 +0700

    add main application

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Intelligent inbox sorting using machine learning for email classification"""
+
+class App:
+    def __init__(self):
+        self.config = {}
+    
+    def run(self):
+        print("Running inbox-smart-sort...")
+        return True
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
